b62cecba7af70202f1e21bda99996fc2d4540a78
danyaiteens <danyaiteens@gmail.com>
2020-02-29 22:01:01 +0200

    ddd

---
+++ homework-1/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+let a;
+console.log(a);
+console.log(typeof a);
+
+a = 34;
+console.log(a);
+console.log(typeof a);
+
+let b = "30";
+b = 30;
+console.log(b);
+console.log(typeof b);
+
+let c = false;
+c = true;
+console.log(c);
+console.log(typeof c);
+
+var d = 20;
+console.log(d);
+console.log(typeof d);
+
+const name = "Aleks";
+console.log(name);
+console.log(typeof name);
+// name = "Kate";
+// console.log(name);
+// console.log(typeof name);
+
+const isAuthenticad = true;
+console.log(isAuthenticad);
+console.log(typeof isAuthenticad);
+
+const isModalOpen = false;
+console.log(isModalOpen);
+console.log(typeof isModalOpen);
+
+const fe_group = null;
+console.log(fe_group);
+console.log(typeof fe_group);
+
+const message = "Hello World!";
+console.log(message);
+
+let men = "Ben";
+console.log(men);
+console.log(`${men}, ${message}`);
+console.log("${men}, ${message}");
+
+// alert(`${men}, ${message}`);
+
+// const isReady = confirm("Are you ready to study JavaScript?");
+// console.log(isReady);
+
+const enterName = prompt("Enter your name please!");
+console.log(enterName);
+console.log(typeof enterName);
